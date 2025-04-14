58b3c0c6291e075b8961ccfad48d8cc341f26c70
jin7942 <infomer.jin@gmail.com>
2025-04-15 01:51:17 +0900

    1

--- src/main/java/com/jinfw/infra/usedmarket/common/security/SecurityConfig.java
+++ src/main/java/com/jinfw/infra/usedmarket/common/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -16,32 +17,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  private final JwtAuthenticationFilter jwtAuthenticationFilter;
-  private final CorsConfigurationSource corsConfigurationSource; // WebConfig에서 주입됨
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final CorsConfigurationSource corsConfigurationSource; // WebConfig에서 주입됨
 
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.cors(cors -> cors.configurationSource(corsConfigurationSource)) // CORS 설정 적용
-        .csrf(csrf -> csrf.disable())
-        .sessionManagement(
-            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> auth.requestMatchers("/api/users/auth/login", // 로그인
-            "/api/users/auth/register", // 회원가입
-            "/api/users/auth/check-email", // 이메일 중복 체크
-            "/api/users/auth/check-nickname", // 닉네임 중복 쳌
-            "/api/items/list", // 메인페이지 상품 조회
-            "/swagger-ui.html/**" // api 문서 페이지
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource)) // CORS 설정 적용
+				.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/users/auth/login", // 로그인
+						"/api/users/auth/register", // 회원가입
+						"/api/users/auth/check-email", // 이메일 중복 체크
+						"/api/users/auth/check-nickname", // 닉네임 중복 쳌
+						"/api/items/list", // 메인페이지 상품 조회
+						"/swagger-ui.html/**" // api 문서 페이지
+				).permitAll().requestMatchers("/api/admin/**").hasRole("ADMIN").anyRequest().authenticated())
+				.httpBasic(httpBasic -> httpBasic.disable())
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        ).permitAll() // 모든 요청 허용(개발용)
-            .requestMatchers("/api/admin/**").hasRole("ADMIN").anyRequest().authenticated())
-        .httpBasic(httpBasic -> httpBasic.disable())
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
 
-    return http.build();
-  }
-
-  @Bean // 비밀번호 암호화
-  BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+	@Bean // 비밀번호 암호화
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
