e2527516c311f970f4d53bd07998a9efcbca3fd6
jin7942 <infomer.jin@gmail.com>
2025-06-26 01:04:20 +0900

    style:Update default setting

--- src/main/java/com/jinfw/infra/usedmarket/user/entity/User.java
+++ src/main/java/com/jinfw/infra/usedmarket/user/entity/User.java
@@ -3,6 +3,7 @@ package com.jinfw.infra.usedmarket.user.entity;
 import com.jinfw.infra.usedmarket.common.base.BaseEntity;
 import com.jinfw.infra.usedmarket.common.constants.CommonCode.UserRoleCode;
 import com.jinfw.infra.usedmarket.common.constants.CommonCode.UserStatusCode;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -25,36 +26,36 @@ import lombok.Setter;
 @Table(name = "user")
 public class User extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "seq")
-  private int seq; // PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "seq")
+	private int seq; // PK
 
-  // 유저 이메일
-  @Column(name = "userEmail", nullable = false, unique = true)
-  private String userEmail;
+	// 유저 이메일
+	@Column(name = "userEmail", nullable = false, unique = true)
+	private String userEmail;
 
-  // 유저 비밀번호
-  @Column(name = "userPassword", nullable = false)
-  private String userPassword;
+	// 유저 비밀번호
+	@Column(name = "userPassword", nullable = false)
+	private String userPassword;
 
-  // 유저 별명
-  @Column(name = "userNickname", nullable = false)
-  private String userNickname;
+	// 유저 별명
+	@Column(name = "userNickname", nullable = false)
+	private String userNickname;
 
-  // 유저 프로필 이미지 URL
-  // 기본 이미지 지정
-  @Column(name = "userProfileImg", nullable = true)
-  private String userProfileImg = "http://localhost:4000/upload/baseImg.png";
+	// 유저 프로필 이미지 URL
+	// 기본 이미지 지정
+	@Column(name = "userProfileImg", nullable = true)
+	private String userProfileImg = "https://nextuse.jin7942.co.kr/upload/baseImg.png";
 
-  // 유저 권한(공통 코드)
-  @Enumerated(EnumType.STRING)
-  @Column(name = "userRoleCode", nullable = false)
-  private UserRoleCode userRoleCode = UserRoleCode.USER;
+	// 유저 권한(공통 코드)
+	@Enumerated(EnumType.STRING)
+	@Column(name = "userRoleCode", nullable = false)
+	private UserRoleCode userRoleCode = UserRoleCode.USER;
 
-  // 유저 상태
-  @Enumerated(EnumType.STRING)
-  @Column(name = "userStateCode", nullable = false)
-  private UserStatusCode userStateCode = UserStatusCode.ACTIVE;
+	// 유저 상태
+	@Enumerated(EnumType.STRING)
+	@Column(name = "userStateCode", nullable = false)
+	private UserStatusCode userStateCode = UserStatusCode.ACTIVE;
 
 }
